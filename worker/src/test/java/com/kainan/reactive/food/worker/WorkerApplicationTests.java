03809b115ffdc13026375d91086925da15eddbf1
KainanPR <kainanpr95@gmail.com>
2021-04-06 19:51:30 -0300

    Added the worker module

---
+++ worker/src/test/java/com/kainan/reactive/food/worker/WorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kainan.reactive.food.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
