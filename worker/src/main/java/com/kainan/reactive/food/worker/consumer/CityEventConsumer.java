e1d9c3d6b73b724ba0cfba2d4f444cb720598b73
KainanPR <kainanpr95@gmail.com>
2021-04-07 00:33:50 -0300

    added reactive kafka in the worker

--- worker/src/main/java/com/kainan/reactive/food/worker/consumer/CityEventConsumer.java
+++ worker/src/main/java/com/kainan/reactive/food/worker/consumer/CityEventConsumer.java
@@ -1,24 +1,27 @@
 package com.kainan.reactive.food.worker.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kainan.reactive.food.infrastructure.kafka.event.CityEvent;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import reactor.kafka.receiver.KafkaReceiver;
 
-@Component
+@Configuration
 @AllArgsConstructor
 @Slf4j
 public class CityEventConsumer {
-    private final ObjectMapper objectMapper;
+    private final KafkaReceiver<String, CityEvent> kafkaReceiver;
 
-    @KafkaListener(
-            topics = "${kafka.topics.city-event.name}",
-            properties = "max.poll.interval.ms=${kafka.max-poll-interval-ms}"
-    )
-    public void consume(ConsumerRecord<String, CityEvent> message) {
-        log.info("message consumed - key: {}, value: {}", message.key(), message.value());
+    @EventListener(ApplicationReadyEvent.class)
+    public void consumer() {
+        kafkaReceiver
+                .receive()
+                .doOnNext(message -> {
+                    log.info("message consumed - message: {}", message);
+                    message.receiverOffset().acknowledge();
+                })
+                .subscribe();
     }
 }
